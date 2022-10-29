56e3b70dcf30340a6b05270342eef7a57628840b
Orlundo Hubbard <orlundokhubbard@gmail.com>
2022-10-28 21:24:06 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const tableBody = document.getElementById("table-body");
+
+let flights = [
+  {
+    time: "08:11",
+    destination: "OMAN",
+    flight: "OX 203",
+    gate: " A 01",
+    remarks: "ON TIME"
+  },
+
+  {
+    time: 
+  }
+]
